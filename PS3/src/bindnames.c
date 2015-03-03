7bc55f113360792d6f3d79a75e974194c4f06607
Yulong Bai <yulongb@stud.ntnu.no>
2015-03-03 20:01:46 +0100

    stage 6 done

--- PS3/src/bindnames.c
+++ PS3/src/bindnames.c
@@ -4,17 +4,34 @@ char* thisClass;
 
 int bind_default ( node_t *root, int stackOffset)
 {
-
+	if(root == NULL)
+		return 0;
+	for (int i = 0; i < root->n_children; ++i) {
+		if(root->children[i] != NULL)
+		{
+			root->children[i]->bind_names(root->children[i], stackOffset +1);
+		}
+	}
+	return 0;
 }
 
 
 
 int bind_constant ( node_t *root, int stackOffset)
 {
+	if(root == NULL)
+		return 0;
+	for (int i = 0; i < root->n_children; ++i) {
+		if(root->children[i] != NULL)
+		{
+			root->children[i]->bind_names(root->children[i], stackOffset +1);
+		}
+	}
+
 	if(outputStage == 6)
 		printf( "CONSTANT\n");
 
-
+	return 0;
 }
 
 
