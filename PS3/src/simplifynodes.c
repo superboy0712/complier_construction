451486c2f8d78e7b242aa5e16fce1626b024381a
Yulong Bai <yulongb@stud.ntnu.no>
2015-03-03 17:25:24 +0100

    some improvement, maybe need to rewrite the whole function

--- PS3/src/simplifynodes.c
+++ PS3/src/simplifynodes.c
@@ -178,17 +178,15 @@ Node_t *simplify_single_child ( Node_t *root, int depth )
 Node_t *simplify_list_with_null ( Node_t *root, int depth )
 {
 	if(root == NULL){
-		return NULL;
-	}
-	if(outputStage == 4)
-		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
+			return NULL;
+		}
+//	if(outputStage == 4)
+//		printf( "%*cSimplify %s \n", depth, ' ', root->nodetype.text );
 
 	for( int i = 0; i < root->n_children; i++){
-		if(root->children[i] != NULL){
-			//simplify_list_with_null(root->children[i], depth + 1);
-			// brilliant here, don't recurse in the wrong way
-			root->children[i]->simplify(root->children[i], depth + 1);
-		}else if(root->children[i + 1] != NULL){
+
+		if( (root->children[i] == NULL)
+				&&(root->children[i + 1] != NULL)){
 			// move the successor elements one step ahead, reallocate the size
 			if(memmove(&(root->children[i]), &(root->children[i + 1]), (root->n_children - i -1)*sizeof(Node_t *) ) == NULL){
 				perror("memmove");
@@ -208,9 +206,15 @@ Node_t *simplify_list_with_null ( Node_t *root, int depth )
 
 		}
 	}
+	/* normal list list simplification */
+	simplify_list(root, depth);
 	/* function_list may also need to eliminate single child situation */
-	if((root->children[0]->n_children != 1)
-			||(root->children[0]->nodetype.index != function_list_n.index))
+	if(
+			//((root->children[0]->n_children == 1)
+			//&&(root->children[0]->nodetype.index == function_list_n.index))/*exclude the highest level one */
+				(root->n_children !=1)
+				||(root->nodetype.index != function_list_n.index)
+				|| depth ==1 )/* reserve the 1 lvl function_list node*/
 			return root;
 
 	/* copy the whole content of the child to me */
@@ -237,7 +241,6 @@ Node_t *simplify_list_with_null ( Node_t *root, int depth )
 Node_t *simplify_list ( Node_t *root, int depth )
 {
 	if(root == NULL){
-			perror("simplify_list, null node");
 			return NULL;
 		}
 	if(outputStage == 4)
