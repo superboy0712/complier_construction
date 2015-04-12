e10157e929ebf645c6d1f24b566c81287155c762
Yulong Bai <yulongb@stud.ntnu.no>
2015-04-12 21:42:30 +0200

    new version of separation works in simple ways

--- PS5/src/generator.c
+++ PS5/src/generator.c
@@ -335,54 +335,10 @@ void gen_ARRAY_INDEX_e_address_calculation(node_t *root){
 void gen_ARRAY_INDEX_rvalue(node_t *root){
 	assert(root);
 	assert(root->expression_type.index == ARRAY_INDEX_E);
-	/**
-	 * var[X][Y][Z]
-	 *
-	 * idx-expr(Z)
-	 * 	\idx-expr(Y)
-	 * 	  \idx-expr(X)
-	 * 	    \variable(var)
-	 *
-	 * 	now expr := INT_CONST, NOT YET including arithmetic expressions
-	 */
-	if(root->children[0]->nodetype.index != variable_n.index){
-		gen_ARRAY_INDEX_e_address_calculation(root->children[0]);
-		/* now r0 stores the address of var[X]/left child
-		 * stack top is value of var[X]/left child */
-		tracePrint("#####generate right child of index in middle#####\n");
-		gen_SUB_tree(root->children[1], 0);// generate Y
-		tracePrint("#####end right child of index in middle#####\n");
-
-		instruction_add(POP, r2, NULL, 0, 0); // r2 <= Y
-		instruction_add(POP, r3, NULL, 0, 0); // r3 <= var[X]
-		instruction_add(POP, r0, NULL, 0, 0); // r0 <= address of var[X]
-		instruction_add3(LSL, r2, r2, STRDUP("#2"));// r2 < 4*r2
-		instruction_add3(ADD, r0, r3, r2);// r0 <= r3 + r2
-		/* now r0 stores the address of var[X][Y] */
-		/* try to protect r0 */
-		instruction_add(PUSH, r0, NULL, 0, 0);
-		instruction_add(LDR, r3, r0, 0, 0);
-		instruction_add(PUSH, r3, NULL, 0, 0);/*!< PUSH the rvalue on top of stack */
-	}else{
-		assert(root->children[0]->nodetype.index == variable_n.index);
-		tracePrint("#####generate left child of index#####\n");
-		gen_VARIABLE(root->children[0], 0);// generate var
-		tracePrint("#####end left child of index#####\n");
-		tracePrint("#####generate right child of index#####\n");
-		gen_SUB_tree(root->children[1], 0);// generate X
-		tracePrint("#####end right child of index#####\n");
-
-		instruction_add(POP, r2, NULL, 0, 0); // r2 <= X
-		instruction_add(POP, r3, NULL, 0, 0); // r3 <= var
-		instruction_add3(LSL, r2, r2, STRDUP("#2"));// r2 < 4*r2
-		instruction_add3(ADD, r0, r3, r2);// r0 <= r3 + r2
-		/* now r0 stores the address of var[X] */
-		/* try to protect r0 */
-		instruction_add(PUSH, r0, NULL, 0, 0);
-		instruction_add(LDR, r3, r0, 0, 0);
-		instruction_add(PUSH, r3, NULL, 0, 0);/*!< PUSH the rvalue on top of stack */
-	}
-
+	gen_ARRAY_INDEX_e_address_calculation(root);
+	instruction_add(POP, r0, NULL, 0, 0); // r0 <= idx's address
+	instruction_add(LDR, r0, r0, 0, 0);
+	instruction_add(PUSH, r0, NULL, 0, 0); // push on top of stack
 }
 void gen_EXPRESSION(node_t *root, int scopedepth) {
 	tracePrint("Starting EXPRESSION of type %s\n",
@@ -477,7 +433,7 @@ void gen_EXPRESSION(node_t *root, int scopedepth) {
 			/** TODO why not flatten/simplify array_index_expression? */
 			assert(root->n_children==2);
 			assert(root->children[0]);
-			gen_ARRAY_INDEX_e_address_calculation(root);
+			gen_ARRAY_INDEX_rvalue(root);
 		}
 		break;
 		default:
@@ -573,7 +529,6 @@ void gen_ASSIGNMENT_STATEMENT(node_t *root, int scopedepth) {
 	assert(root);
 	assert(root->n_children == 2);
 	assert(root->nodetype.index == assignment_statement_n.index);
-	//gen_default(root, scopedepth); // push rvalue
 	tracePrint("#####  right child of assign#####\n");
 	gen_SUB_tree(root->children[1], scopedepth);
 	tracePrint("#####  end right child of assign#####\n");
@@ -592,17 +547,9 @@ void gen_ASSIGNMENT_STATEMENT(node_t *root, int scopedepth) {
 		/**
 		 *  lvalue is array_index_expression
 		 */
-		//! TODO WARNING, WOULD MAKE incomplete INDEX modifiable, destroy the array's weaving
-		tracePrint("#####  left child gen of index in assign#####\n");
 		gen_ARRAY_INDEX_e_address_calculation(root->children[0]);
-		tracePrint("##### END left child gen of index in assign#####\n");
-		/**
-		 *  now, r0 holds index's address
-		 */
-		/* now r3 was polluted !!!*/
 		/* acquire rvalue */
 		instruction_add(POP, r0, NULL, 0, 0); /*! address */
-		instruction_add(POP, r5, NULL, 0, 0); /*< lvalue */
 		instruction_add(POP, r5, NULL, 0, 0); /*! rvalue */
 		instruction_add(STR, r5, r0, 0, 0);
 	}
